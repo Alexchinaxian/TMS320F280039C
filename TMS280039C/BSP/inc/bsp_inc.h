620cc0bb3ef2d710581a2be39d2801723f3fe851
xiaohexin <xiaohexin@cvte.com>
2025-01-14 17:58:45 +0800

    1

---
+++ TMS280039C/BSP/inc/bsp_inc.h
@@ -0,0 +1,12 @@
+#ifndef BSP_BSP_INC_H_
+#define BSP_BSP_INC_H_
+
+//库文件
+#include "Driver/inc/f28003x_sysctrl.h"
+
+
+//BSP文件
+#include "BSP/inc/bsp_F280039C_Init.h"
+
+
+#endif /* BSP_BSP_INC_H_ */
